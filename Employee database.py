55d7e48b915b811111e074a9f4310d99aec8df39
K Harikrisshna <73019506+Harikrisshna@users.noreply.github.com>
2023-06-22 22:39:40 +0530

    Update Employee database.py

--- Employee database.py
+++ Employee database.py
@@ -1,20 +1,33 @@
 #WAMDP to perform push,pop,display operation on stack containing employee details and given in following structure
 #Employee no. and name, program should have push, pop, display
 def push(employee):
-    
-a=int(input("1.Add Employee\n2.delete Employee\n3.View Employee details"))
-
+    x=input("Enter name:")
+    y=input("Enter employee number:")
+    employee.append([x,y])
+def pop(employee):
+    if employee==[]:
+        print("Underflow")
+    else:
+        a=employee.pop()
+        print("Employee name:",a[0],"\nEmployee number:",a[1])
+def display(employee):
+    if employee==[]:
+        print("Underflow")
+    else:
+        print(employee[-1],"<-top")
+        for i in range(len(employee)-2,-1,-1):
+            print(employee[i][0],employee[i][1])
 employee=list()
-
-while p=1:
+c='y'
+while c=='y' or c=='Y':
+    a=int(input("1.Add Employee\n2.Delete Employee\n3.Employee details\nEnter choice:"))
     if a==1:
-        name=input("Enter empoloyee name:")
-        num=int(input("Enter employee number:"))
-        push(employee,(name,num))
+        push(employee)
     elif a==2:
         pop(employee)
     elif a==3:
         display(employee)
-p=int(input("Press 1 to continue"))
-if p!=1:
-    print("Thank you")
+    c=input("Do you want to continue?Y/N")
+    if c=='n' or c=='N':
+        print("Thank you")
+        break
